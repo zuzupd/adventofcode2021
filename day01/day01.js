be365084b68e8e94a0752d85e4fd5d9964d51a0c
Ondrej Dovec <ondrej.dovec@saboit.de>
2021-12-01 19:46:50 +0100

    Day 01 Part 2

--- day01/day01.js
+++ day01/day01.js
@@ -1,23 +1,54 @@
-import {depths} from "./depths.js"
+import { depths } from "./depths.js";
 
-let inc
-const test1 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-const test2 = [1, 2, 3, 4, 5]
-const test3 = [5, 4, 3, 2, 1]
-let prev
+const test1 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+const test2 = [1, 2, 3, 4, 5];
+const test3 = [5, 4, 3, 2, 1];
 
-function sonar (val) {
-    inc = 0
-    prev = val[0]
-    val.forEach(ele => {
-        if(prev < ele) inc++
-        prev = ele
-    });
-    return(inc)
+function sonar(val) {
+  let inc = 0;
+  let prev = val[0];
+
+  val.forEach((ele) => {
+    if (prev < ele) inc++;
+    prev = ele;
+  });
+  return inc;
 }
 
-console.log("Test result: "+ (sonar(test1) == 7) + " value is: " + inc)
-console.log("Test result: "+ (sonar(test2) == 4) + " value is: " + inc)
-console.log("Test result: "+ (sonar(test3) == 0) + " value is: " + inc)
+function band(val) {
+  let bands = [];
 
-console.log("The number of times a depth measurement increases: " + sonar(depths)) 
+  for (let i = 0; i + 2 < val.length; i++) {
+    bands.push(val[i] + val[i + 1] + val[i + 2]);
+  }
+  return bands;
+}
+
+function sonarTest(val, expected) {
+  console.log("test result for sonar function: " + (sonar(val) == expected));
+}
+
+function bandTest(val, length, expected) {
+  console.log("test result is for band function: " + (band(val).length == length));
+  sonarTest(band(val), expected);
+}
+
+console.log("Tests:")
+
+sonarTest(test1, 7);
+sonarTest(test2, 4);
+sonarTest(test3, 0);
+bandTest(test1, 8, 5);
+bandTest(test2, 3, 2);
+bandTest(test3, 3, 0);
+console.log("")
+
+console.log("--- Day 1: Sonar Sweep ---")
+
+console.log(
+  "Part 1: The number of times a depth measurement increases: " + sonar(depths)
+);
+console.log(
+  "Part 2: The number of times the sum of measurements in sliding windows increases: " +
+    sonar(band(depths))
+);
