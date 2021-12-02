1e8a72d892dcbc8bfbba4bbc519d182a55d7e503
Ondrej Dovec <ondrej.dovec@saboit.de>
2021-12-02 12:25:28 +0100

    prettify code

--- day01/day01.js
+++ day01/day01.js
@@ -1,4 +1,4 @@
-import { depths } from "./depths.js";
+import { depths } from './depths.js';
 
 const test1 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 const test2 = [1, 2, 3, 4, 5];
@@ -25,15 +25,20 @@ function band(val) {
 }
 
 function sonarTest(val, expected) {
-  console.log("test result for sonar function: " + (sonar(val) == expected));
+  console.log(
+    'test result for sonar function: ' + (sonar(val) == expected),
+  );
 }
 
 function bandTest(val, length, expected) {
-  console.log("test result is for band function: " + (band(val).length == length));
+  console.log(
+    'test result is for band function: ' +
+      (band(val).length == length),
+  );
   sonarTest(band(val), expected);
 }
 
-console.log("Tests:")
+console.log('Tests:');
 
 sonarTest(test1, 7);
 sonarTest(test2, 4);
@@ -41,14 +46,15 @@ sonarTest(test3, 0);
 bandTest(test1, 8, 5);
 bandTest(test2, 3, 2);
 bandTest(test3, 3, 0);
-console.log("")
+console.log('');
 
-console.log("--- Day 1: Sonar Sweep ---")
+console.log('--- Day 1: Sonar Sweep ---');
 
 console.log(
-  "Part 1: The number of times a depth measurement increases: " + sonar(depths)
+  'Part 1: The number of times a depth measurement increases: ' +
+    sonar(depths),
 );
 console.log(
-  "Part 2: The number of times the sum of measurements in sliding windows increases: " +
-    sonar(band(depths))
+  'Part 2: The number of times the sum of measurements in sliding windows increases: ' +
+    sonar(band(depths)),
 );
--- day02/day02.js
+++ day02/day02.js
@@ -1,30 +1,30 @@
-import { directions } from "./directions.js";
+import { directions } from './directions.js';
 
 const test1 = [
-  "forward 5",
-  "down 5",
-  "forward 8",
-  "up 3",
-  "down 8",
-  "forward 2",
+  'forward 5',
+  'down 5',
+  'forward 8',
+  'up 3',
+  'down 8',
+  'forward 2',
 ];
 const test2 = [
-  "forward 1",
-  "down 1",
-  "up 1",
-  "up 1",
-  "down 1",
-  "up 1",
-  "down 1",
-  "up 1",
+  'forward 1',
+  'down 1',
+  'up 1',
+  'up 1',
+  'down 1',
+  'up 1',
+  'down 1',
+  'up 1',
 ];
 const test3 = [
-    "down 6",
-    "forward 5",
-    "up 1",
-    "forward 5",
-    "up 1",
-    "down 6",
+  'down 6',
+  'forward 5',
+  'up 1',
+  'forward 5',
+  'up 1',
+  'down 6',
 ];
 
 function navigation(data) {
@@ -32,12 +32,12 @@ function navigation(data) {
   let forward = 0;
 
   data.forEach((element) => {
-    if (element.includes("down"))
-      depth = depth + parseInt(element.replace("down ", ""));
-    if (element.includes("up"))
-      depth = depth - parseInt(element.replace("up ", ""));
-    if (element.includes("forward"))
-      forward = forward + parseInt(element.replace("forward ", ""));
+    if (element.includes('down'))
+      depth = depth + parseInt(element.replace('down ', ''));
+    if (element.includes('up'))
+      depth = depth - parseInt(element.replace('up ', ''));
+    if (element.includes('forward'))
+      forward = forward + parseInt(element.replace('forward ', ''));
   });
 
   let distance = depth * forward;
@@ -45,19 +45,19 @@ function navigation(data) {
 }
 
 function trueNavigation(data) {
-    let aim = 0;
-    let depth = 0;
+  let aim = 0;
+  let depth = 0;
   let forward = 0;
 
   data.forEach((element) => {
-    if (element.includes("down"))
-      aim = aim + parseInt(element.replace("down ", ""));
-    if (element.includes("up"))
-      aim = aim - parseInt(element.replace("up ", ""));
-    if (element.includes("forward")){
-      depth = depth + (aim * parseInt(element.replace("forward ", "")))
-      forward = forward + parseInt(element.replace("forward ", ""));
-      }
+    if (element.includes('down'))
+      aim = aim + parseInt(element.replace('down ', ''));
+    if (element.includes('up'))
+      aim = aim - parseInt(element.replace('up ', ''));
+    if (element.includes('forward')) {
+      depth = depth + aim * parseInt(element.replace('forward ', ''));
+      forward = forward + parseInt(element.replace('forward ', ''));
+    }
   });
 
   let distance = depth * forward;
@@ -66,24 +66,42 @@ function trueNavigation(data) {
 
 function navigationTest(data, expected) {
   console.log(
-    "Test expecting " + expected + " and observed value is " + navigation(data)
+    'Test expecting ' +
+      expected +
+      ' and observed value is ' +
+      navigation(data),
   );
-  console.log("Test result for simple navigation is " + (expected == navigation(data)));
+  console.log(
+    'Test result for simple navigation is ' +
+      (expected == navigation(data)),
+  );
 }
 
 function trueNavigationTest(data, expected) {
-    console.log(
-      "Test expecting " + expected + " and observed value is " + trueNavigation(data)
-    );
-    console.log("Test result for true navigation is " + (expected == trueNavigation(data)));
-  }
+  console.log(
+    'Test expecting ' +
+      expected +
+      ' and observed value is ' +
+      trueNavigation(data),
+  );
+  console.log(
+    'Test result for true navigation is ' +
+      (expected == trueNavigation(data)),
+  );
+}
 
 navigationTest(test1, 150);
 navigationTest(test2, -1);
-navigationTest(test3, 100)
-trueNavigationTest(test1, 900)
-console.log("")
+navigationTest(test3, 100);
+trueNavigationTest(test1, 900);
+console.log('');
 
-console.log("--- Day 2: Dive! ---")
-console.log("Part 1: What do you get if you multiply your final horizontal position by your final depth? " + navigation(directions))
-console.log("Part 2: What do you get if you multiply your final horizontal position by your final depth? " + trueNavigation(directions))
+console.log('--- Day 2: Dive! ---');
+console.log(
+  'Part 1: What do you get if you multiply your final horizontal position by your final depth? ' +
+    navigation(directions),
+);
+console.log(
+  'Part 2: What do you get if you multiply your final horizontal position by your final depth? ' +
+    trueNavigation(directions),
+);
