a3296ceb54d6cde6ac71d76d5f6aaad305da343d
Ondrej Dovec <ondrej.dovec@saboit.de>
2021-12-03 20:13:30 +0100

    day 03 part 2

--- day03/day03.js
+++ day03/day03.js
@@ -4,12 +4,20 @@ import { report } from './reportdata.js';
 import {
   powerConsumption,
   powerConsumptionTest,
+  lifeSupportRating,
+  lifeSupportTest,
 } from './diagnostic.js';
 
 test(powerConsumptionTest(test1, 198));
 test(powerConsumptionTest(test2, 2230856));
+test(lifeSupportTest(test1, 230));
+test(lifeSupportTest(test1, 230));
 
 console.log(
   'Part 1: What is the power consumption of the submarine? ' +
     powerConsumption(report),
+);
+console.log(
+  'What is the life support rating of the submarine? ' +
+    lifeSupportRating(report),
 );
--- day03/diagnostic.js
+++ day03/diagnostic.js
@@ -40,16 +40,70 @@ function modeTwo(data) {
     );
   });
 
+  if (occur[0] == occur[1]) return [1, 0];
   const max = occur.indexOf(Math.max(occur[0], occur[1]));
   const min = occur.indexOf(Math.min(occur[0], occur[1]));
 
   return [max, min];
 }
 
+function gasSearch(data, type) {
+  let set;
+  switch (type) {
+    case 'o2':
+      set = 0;
+      break;
+    case 'co2':
+      set = 1;
+  }
+  let length = data.split('\n')[1].length;
+  let filteredData = data;
+  let regex = Array.apply(null, Array(length)).map(function () {
+    return '.';
+  });
+  let result;
+
+  for (let i = 0; i < length; i++) {
+    (result = modeTwo(
+      prepareArray(
+        filteredData.replace(/(\r\n|\n|\r)/gm, '').split(''),
+        length,
+      )[i],
+    )),
+      (regex[i] = result[set]);
+    filteredData = data
+      .match(new RegExp(`^${regex.join('')}$`, 'gm'))
+      .join('\n');
+    if (filteredData.length == length) {
+      return filteredData;
+    }
+  }
+}
+
+export function lifeSupportRating(data) {
+  const o2 = gasSearch(data, 'o2');
+  const co2 = gasSearch(data, 'co2');
+
+  const result = parseInt(o2, 2) * parseInt(co2, 2);
+
+  return result;
+}
+
 export function powerConsumptionTest(data, expected) {
   const observed = powerConsumption(data);
   const msg =
     'diagnostic - expected value is: ' +
+    expected +
+    ' observed value is: ' +
+    observed;
+  const result = observed == expected;
+  return { result, msg };
+}
+
+export function lifeSupportTest(data, expected) {
+  const observed = lifeSupportRating(data);
+  const msg =
+    'life support - expected value is: ' +
     expected +
     ' observed value is: ' +
     observed;
